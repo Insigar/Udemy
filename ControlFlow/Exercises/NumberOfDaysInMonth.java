58c201adabf8a12c1b635b676cc46d10a13c9835
Insigar <90148640+Insigar@users.noreply.github.com>
2024-12-02 13:48:34 +0100

    Update NumberOfDaysInMonth.java
    Explored some different ways to accomplish the same in NumberOfDaysInMonth

--- ControlFlow/Exercises/NumberOfDaysInMonth.java
+++ ControlFlow/Exercises/NumberOfDaysInMonth.java
@@ -27,6 +27,18 @@ public class NumberOfDaysInMonth
 		return false;
 	}
 
+	// public static int returnFeb(int year)
+	// {
+	// 	if (isLeapYear(year))
+	// 	{
+	// 		return 29;
+	// 	}
+	// 	else
+	// 	{
+	// 		return 28;
+	// 	}
+	// }
+
 	public static int getDaysInMonth(int month, int year)
 	{
 		if (month < 1 || month > 12)
@@ -37,6 +49,7 @@ public class NumberOfDaysInMonth
 		{
 			return -1;
 		}
+
 		return switch (month)
 		{
 			case 1, 3, 5, 7, 8, 10, 12 -> 31;
@@ -50,8 +63,36 @@ public class NumberOfDaysInMonth
 					yield 28;
 				}
 			}
-			// case 4,6,9,11 -> 30;
+			// case 2 -> returnFeb(year);
 			default -> 30;
 		};
+
+		// int days = 30;
+
+		// switch (month)
+		// {
+		// 	case 1:
+		// 	case 3:
+		// 	case 5:
+		// 	case 7:
+		// 	case 8:
+		// 	case 10:
+		// 	case 12:
+		// 		days = 31;
+		// 		break;
+		// 	case 2:
+		// 		if (isLeapYear(year))
+		// 		{
+		// 			days = 29;
+		// 		}
+		// 		else
+		// 		{
+		// 			days = 28;
+		// 		}
+		// 		break;
+		// 	default:
+		// 		break;
+		// }
+		// return days;
 	}
 }
