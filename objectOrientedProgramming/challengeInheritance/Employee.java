64eebe396c5b3326dd0096065b5424aaa3092491
Insigar <90148640+Insigar@users.noreply.github.com>
2025-02-18 10:24:59 +0100

    Temp fix for missing constructor

--- objectOrientedProgramming/challengeInheritance/Employee.java
+++ objectOrientedProgramming/challengeInheritance/Employee.java
@@ -5,6 +5,11 @@ public class Employee extends Worker
 	private long employeeId;
 	private String hireDate;
 
+	public Employee()
+	{
+
+	}
+
 	public Employee(String name, String birthDate, long employeeId, String hireDate)
 	{
 		super(name, birthDate);
